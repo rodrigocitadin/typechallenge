370b4f1fa5badc56ea35d7a81eed3581cab46bb2
rodrigocitadin <citadinrodrigo@gmail.com>
2025-02-25 20:09:18 -0300

    ts pick

---
+++ challenges/pick.ts
@@ -0,0 +1,16 @@
+type MyPick<T, K extends keyof T> = {
+  [k in K]: T[k]
+}
+
+interface Todo {
+  title: string
+  description: string
+  completed: boolean
+}
+
+type TodoPreview = MyPick<Todo, 'title' | 'completed'>
+
+const todo: TodoPreview = {
+  title: 'Clean room',
+  completed: false,
+}
